9229927d33251f429bfd34fb1e0ad09e59ba027c
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2022-03-11 19:45:25 +0000

    Add project files.

---
+++ TicTacToeGame/Scripts/react/Import.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            Hello I am react app.
+        </div>
+    );
+}
+
+export default App;
